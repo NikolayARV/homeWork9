dba945ef64b1a0b3eedd9a34da1a77d0b19dc624
razenkov <razenkov.rna@mail.ru>
2022-10-15 16:58:14 +0300

    Добавил генерацию массива со случайными числами

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int[] expenses = new int[30];
-        expenses[0] = 100000;
-        for (int i = 1; i < expenses.length; i++) {
-            expenses[i] = expenses[i-1] + 3000;
-        }
+        int[] expenses = generateRandomArray();
+
         int sum = 0;
         for (int expens : expenses) {
             sum += expens;
         }
-            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
         System.out.println("Задание 2");
         int min = expenses[25];
@@ -24,20 +21,26 @@ public class Main {
             if (max < expens)
                 max = expens;
         }
-                System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей");
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей");
         System.out.println("Задание 3");
         System.out.println("Средняя сумма трат за месяц составила " + sum / expenses.length + " рублей");
         System.out.println("Задание 4"); // Ivanovich
         char[] reverseFullName = {'h', 'c', 'i', 'v', 'o', 'n', 'a', 'v', 'I', ' ', 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--)
-        {
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
 
-
     }
 
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
         }
+        return arr;
+    }
+}
 
 
 
